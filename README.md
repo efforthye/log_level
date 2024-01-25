f320e5fe4b84f3f84998d67e27a93a47b4b0c11e
efforthye <efforthye@gmail.com>
2024-01-26 02:15:52 +0900

    feat: add winston logger

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # log_level
+- winston: https://inpa.tistory.com/entry/NODE-%F0%9F%93%9A-Winston-%EB%AA%A8%EB%93%88-%EC%82%AC%EC%9A%A9%EB%B2%95-%EC%84%9C%EB%B2%84-%EB%A1%9C%EA%B7%B8-%EA%B4%80%EB%A6%AC
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import { logger } from './logger';
 
 const fileName: any = __filename.match(/\/([^\/]+)\/?$/);
+dotenv.config();
 
 const app = express();
 const port = 3000;
@@ -11,4 +14,13 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
   console.log(fileName[1], ":", `Server is running on port ${port}`);
+
+  logger.info("hello world");
+  logger.error("hello world");
+  logger.warn("hello world");
+  logger.debug("hello world");
+  logger.verbose("hello world");
+  logger.silly("hello world");
+
+  logger.info('naver profile : ', { message: "hello world~~" });
 });
